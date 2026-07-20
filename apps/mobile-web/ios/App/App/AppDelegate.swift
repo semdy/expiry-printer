7917168c704c467ebdfdbbd235fec6c9e416be77
semdy <501859275@qq.com>
2026-07-20 18:09:02 +0800

    chore: update

--- apps/mobile-web/ios/App/App/AppDelegate.swift
+++ apps/mobile-web/ios/App/App/AppDelegate.swift
@@ -6,7 +6,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MyViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
