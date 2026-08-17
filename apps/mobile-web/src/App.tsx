4b73a875bc4d6a3ccb7e20d6a1297e04c88e7fc5
semdy <501859275@qq.com>
2026-08-17 15:11:26 +0800

    chore: update deps

--- apps/mobile-web/src/App.tsx
+++ apps/mobile-web/src/App.tsx
@@ -1,6 +1,5 @@
 import { ApiProvider } from 'ims-data'
 import { getCurrentLang } from 'shared/i18n'
-import { ThemeProvider } from 'shared/theme'
 import { promisify } from 'shared/utils'
 import MobileApp from './MobileApp'
 
@@ -21,9 +20,7 @@ async function getHeader() {
 export default function App() {
   return (
     <ApiProvider getHeaders={getHeader}>
-      <ThemeProvider>
-        <MobileApp />
-      </ThemeProvider>
+      <MobileApp />
     </ApiProvider>
   )
 }
